ddda0de3eab38256db7f51f28fe3742408c44a88
Akash-Pateria <apateri@ncsu.edu>
2020-11-25 18:52:31 -0500

    Fix typo

--- bigchaindb/web/views/base.py
+++ bigchaindb/web/views/base.py
@@ -56,7 +56,7 @@ def validate_schema_definition(request):
     # `content-type` header is not set to `application/json`
     tx = request.get_json(force=True)
     tx_obj, error = None, None
-    tx["metdata"]["server_ts"] = time.monotonic()
+    tx["metadata"]["server_ts"] = time.monotonic()
     logger.debug(tx)
     try:
         tx_obj = Transaction.from_dict(tx)
