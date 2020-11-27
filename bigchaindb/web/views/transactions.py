dd7dea3ed27b0e494e36a843df1ddfa995839ad3
Akash-Pateria <apateri@ncsu.edu>
2020-11-27 14:49:41 -0500

    Change broadcast mode to BROADCAST_TX_SYNC

--- bigchaindb/web/views/transactions.py
+++ bigchaindb/web/views/transactions.py
@@ -103,7 +103,7 @@ class TransactionListApi(Resource):
         """
         parser = reqparse.RequestParser()
         parser.add_argument(
-            "mode", type=parameters.valid_mode, default=BROADCAST_TX_COMMIT
+            "mode", type=parameters.valid_mode, default=BROADCAST_TX_SYNC
         )
         args = parser.parse_args()
         mode = str(args["mode"])
@@ -122,12 +122,7 @@ class TransactionListApi(Resource):
                     400, "Invalid transaction ({}): {}".format(type(e).__name__, e)
                 )
             else:
-                status_code, message = bigchain.write_transaction(
-                    tx_obj,
-                    BROADCAST_TX_COMMIT
-                    if tx_obj.operation is Transaction.ACCEPT
-                    else mode,
-                )
+                status_code, message = bigchain.write_transaction(tx_obj, mode)
             if status_code == 202 and tx_obj.operation == Transaction.ACCEPT:
                 tx_obj.trigger_transfers(bigchain)
 
