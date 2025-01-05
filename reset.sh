08548b13e3439483aa189c29415748298cf8e4fa
David L. Adei <lokingdav@gmail.com>
2025-01-05 03:11:04 +0000

    Changed mv to cp

--- reset.sh
+++ reset.sh
@@ -6,7 +6,7 @@ docker-compose down
 
 rm -rf $TM/data
 mkdir $TM/data
-mv $TM/priv_validator_state.json.backup $TM/data/priv_validator_state.json
+cp $TM/priv_validator_state.json.backup $TM/data/priv_validator_state.json
 sudo chmod -R 777 $TM/data
 
 docker-compose up -d bigchaindb
